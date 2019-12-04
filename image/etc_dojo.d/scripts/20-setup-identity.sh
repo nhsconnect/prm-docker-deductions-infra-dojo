d41a4dd58360ca59c41dc375404242d39164ffd0
Tomasz Setkowski <tomasz.setkowski@thoughtworks.com>
2019-12-04 13:49:19 +0000

    Redirect dojo setup logs to stderr

--- image/etc_dojo.d/scripts/20-setup-identity.sh
+++ image/etc_dojo.d/scripts/20-setup-identity.sh
@@ -5,27 +5,27 @@
 ###########################################################################
 
 if [ ! -d "${dojo_identity}/.ssh" ]; then
-  echo "${dojo_identity}/.ssh does not exist"
+  >&2 echo "${dojo_identity}/.ssh does not exist"
   mkdir -p ${dojo_home}/.ssh
 else
   cp -r "${dojo_identity}/.ssh/" "${dojo_home}/"
-  find ${dojo_home}/.ssh -name '*id_rsa' -exec chmod -c 0600 {} \;
-  find ${dojo_home}/.ssh -name '*id_rsa' -exec chown dojo:dojo {} \;
+  >&2 find ${dojo_home}/.ssh -name '*id_rsa' -exec chmod -c 0600 {} \;
+  >&2 find ${dojo_home}/.ssh -name '*id_rsa' -exec chown dojo:dojo {} \;
 fi
 # we need to ensure that ${dojo_home}/.ssh/config contains at least:
 # StrictHostKeyChecking no
 echo "StrictHostKeyChecking no
 UserKnownHostsFile /dev/null
 " > "${dojo_home}/.ssh/config"
-chown -R dojo:dojo  ${dojo_home}/.ssh
+>&2 chown -R dojo:dojo  ${dojo_home}/.ssh
 
 # not obligatory configuration file
 if [ -f "${dojo_identity}/.gitconfig" ]; then
   cp "${dojo_identity}/.gitconfig" "${dojo_home}"
-  chown dojo:dojo  ${dojo_home}/.gitconfig
+  >&2 chown dojo:dojo  ${dojo_home}/.gitconfig
 fi
 
 if [[ -d ${dojo_identity}/.aws ]]; then
-    cp -pr ${dojo_identity}/.aws ${dojo_home}/.aws
-    chown dojo:dojo -R ${dojo_home}/.aws
+    >&2 cp -pr ${dojo_identity}/.aws ${dojo_home}/.aws
+    >&2 chown dojo:dojo -R ${dojo_home}/.aws
 fi
